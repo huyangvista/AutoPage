649c1dd81c7147ab747aedbcf19c958e885715ee
Hocean <huyangvista@qq.com>
2017-06-05 14:02:10 +0800

    Initial commit

--- src/vdll/VMode.java
+++ src/vdll/VMode.java
@@ -6,13 +6,19 @@ import org.xutils.db.annotation.Table;
 import org.xutils.ex.DbException;
 import org.xutils.x;
 
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
 import java.util.List;
+import java.util.RandomAccess;
 
 class Main
 {
-	public Main() throws DbException {
-		DbManager db = x.getDb(null);
-		List<VMode> all = db.selector(VMode.class).findAll();
+	public Main() throws DbException, FileNotFoundException {
+		RandomAccessFile rf = new RandomAccessFile("","rw");
+
+
+		//DbManager db = x.getDb(null);
+		//List<VMode> all = db.selector(VMode.class).findAll();
 	}
 }
 
@@ -24,12 +30,12 @@ public class VMode
 	@Column(name = "b")
 	private double b = 10;
 
-	public static void main(String[] args) throws DbException {
+	public static void main(String[] args){
 		//new Main();
+		throw new RuntimeException("服务器请求异常！");
+	}
 
+	public void setA(String a) {
 
 	}
-	
-	
-	
 }
