1a71fb9bb63ee619e2e45a3f2a46cfc4567e5e85
Hocean <huyangvista@qq.com>
2017-06-24 17:18:08 +0800

    Initial commit

---
+++ src/demo/t_user.java
@@ -0,0 +1,17 @@
+package demo;
+
+import vdll.data.msql.AISql;
+
+public class t_user{
+
+@AISql public String id;
+@AISql public String user_name;
+@AISql public String user_phone;
+@AISql public String user_email;
+@AISql public String user_pwd;
+@AISql public String pwd_salt;
+@AISql public String create_time;
+@AISql public String modify_time;
+@AISql public String is_delete;
+
+}
---
+++ src/demo/tc_file.java
@@ -0,0 +1,20 @@
+package demo;
+
+import vdll.data.msql.AISql;
+
+public class tc_file{
+
+@AISql public String id;
+@AISql public String access_url;
+@AISql public String resource_path;
+@AISql public String source_url;
+@AISql public String url;
+@AISql public String vid;
+@AISql public String create_by;
+@AISql public String create_date;
+@AISql public String update_by;
+@AISql public String update_date;
+@AISql public String remarks;
+@AISql public String del_flag;
+
+}
